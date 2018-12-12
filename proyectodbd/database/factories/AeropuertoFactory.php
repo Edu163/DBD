ad33753fa9ad4316be0a9fc3bb41bf90cebbc051
Kömarov <omar.carrasco@usach.cl>
2018-12-12 13:23:36 -0300

    Fix
    Pequeño error de paths

--- proyectodbd/database/factories/AeropuertoFactory.php
+++ proyectodbd/database/factories/AeropuertoFactory.php
@@ -1,9 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
-use App\Aeropuerto;
 
-$factory->define(Aeropuerto::class, function (Faker $faker) {
+$factory->define(App\Aeropuerto::class, function (Faker $faker) {
     return [
         'pais' => $faker->country,
     	'ciudad' => $faker->city,
