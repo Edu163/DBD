2f6aa4ab35d32dbfadd943ec4fb4ce10974e4c3a
Guillermo Campos <guillermo.campos@usach.cl>
2019-01-09 13:55:05 -0300

    Cambios en form de alojamiento

--- proyectodbd/resources/views/welcome.blade.php
+++ proyectodbd/resources/views/welcome.blade.php
@@ -35,7 +35,9 @@
                     @include('modulos.ReservaVehiculo.vehiculos.form')
                 </div>
                 <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"> </div>
-                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"> </div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"> 
+                    @include('modulos.ReservaAlojamiento.Hotel.form')
+                </div>
                 <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"> </div>
             </div>
         </div>
