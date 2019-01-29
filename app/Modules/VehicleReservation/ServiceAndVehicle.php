7559c5c8f2cb8d7518b53cc3c332cbe09d4abeea
ocarrascoe <omar.carrasco@usach.cl>
2019-01-29 11:28:09 -0300

    Cambios de nombre
    Cambios de nombre en los forms, fix en vistas
    Además se agregan gran parte de los modelos de vehicle

--- app/Modules/VehicleReservation/ServiceAndVehicle.php
+++ app/Modules/VehicleReservation/ServiceAndVehicle.php
@@ -1,10 +1,30 @@
 <?php
 
-namespace App;
+namespace App\Modules\VehicleReservation;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceAndVehicle extends Model
 {
-    //
+    protected $table = 'service_and_vehicles';
+
+    protected $servicio_id;
+    protected $patente;
+    protected $precio;
+
+    protected $fillable = [
+        'servicio_id',
+        'patente',
+        'precio',
+    ];
+
+    /* Relaciones */
+
+    public function vehicleReservation(){
+    	return $this->belongsTo(VehicleReservation::class);
+    }
+
+    public function vehicle(){
+    	return $this->belongsTo(Vehicle::class);
+    }
 }
--- app/Modules/VehicleReservation/VehicleProvider.php
+++ app/Modules/VehicleReservation/VehicleProvider.php
@@ -1,10 +1,32 @@
 <?php
 
-namespace App;
+namespace App\Modules\VehicleReservation;
 
 use Illuminate\Database\Eloquent\Model;
 
 class VehicleProvider extends Model
 {
-    //
+    protected $table = 'providers';
+
+    protected $politica_combustible;
+    protected $calificacion;
+    protected $kilometraje;
+    protected $documentacion_necesaria;
+    protected $franquicia_daños;
+    protected $deposito_seguridad;
+
+    protected $fillable = [
+        'politica_combustible',
+        'calificacion',
+        'kilometraje',
+        'documentacion_necesaria',
+        'franquicia_daños',
+        'deposito_seguridad',
+    ];
+
+    /* Relaciones */
+
+    public function vehicle(){
+    	return $this->hasMany(Vehicle::class);
+    }
 }
