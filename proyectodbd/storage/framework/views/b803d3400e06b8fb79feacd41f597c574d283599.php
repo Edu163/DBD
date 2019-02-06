e9be97acc50666810be6d356dd9d60ba115aa5b7
ocarrascoe <omar.carrasco@usach.cl>
2019-02-06 14:10:08 -0300

    Eliminando subproyecto
    Error de arrastre, intentando solucionarlo.

--- proyectodbd/storage/framework/views/b803d3400e06b8fb79feacd41f597c574d283599.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1>
-        No hay disponibles
-    </h1>
-    <a href="\" class="btn btn-danger btn-hoteles" style="vertical-align:middle"><span>Continuar </span> </a>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
