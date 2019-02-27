aa26657236a7c5fca6e8c468a21e71d9d6304db6
Guillermo Campos <guillermo.campos@usach.cl>
2019-02-27 09:58:18 -0300

    Proyecto funcionando en la master

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
             'flightDetails',
             'hotelRooms',
             'vehicleCards',
-            'hotelCards',
+            'hotelCards'
         ));
     }
 }
