c539457867d6f28afb3662a5dde221d7f12b6ef9
Omar Carrasco <omar.carrasco@usach.cl>
2019-03-06 11:44:12 -0300

    Fix JS

--- resources/views/modules/others/package/form.blade.php
+++ resources/views/modules/others/package/form.blade.php
@@ -48,7 +48,7 @@
      var today = new Date();
      var dd = today.getDate();
      var mm = today.getMonth()+1; //January is 0!
-     var yyyy = today.g etFullYear();
+     var yyyy = today.getFullYear();
      if(dd<10){
           dd='0'+dd
      } 
@@ -60,4 +60,3 @@
      document.getElementById("fecha-recogida").setAttribute("min", today);
      document.getElementById("fecha-devolucion").setAttribute("min", today);
 </script>
-<script src="js/funciones.js"></script>
